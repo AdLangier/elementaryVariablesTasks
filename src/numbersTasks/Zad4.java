584b2964abcca7a2260f443ef8460b5718eaa2fb
Adrian Langier <langier.adrian@gmail.com>
2021-03-15 20:16:22 +0100

    additional numbers Task4

---
+++ src/numbersTasks/Zad4.java
@@ -0,0 +1,19 @@
+package numbersTasks;
+
+import java.util.Scanner;
+
+public class Zad4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int amount = 5000;
+        System.out.println("Ile chcesz wypłacić ?");
+        double withdraw = scanner.nextDouble();                    //"nextDouble()" przy klasie Scanner
+        double balance = amount - withdraw;
+
+        if (withdraw > amount) {
+            System.out.println("Niepoprawna operacja");
+        } else {
+            System.out.println("Pozostałe saldo: "+String.format("%.2f", balance)+" PLN");  //wyświetlanie miejsc po przecinku --> Zad13
+        }
+    }
+}
