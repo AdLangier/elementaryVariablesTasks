5cbed853118455a4745f6dc516936cbf1979ae9b
Adrian Langier <langier.adrian@gmail.com>
2021-03-15 21:20:26 +0100

    additional numbers Task11

---
+++ src/numbersTasks/Zad11.java
@@ -0,0 +1,13 @@
+package numbersTasks;
+
+import java.util.Scanner;
+
+public class Zad11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj dokładną cenę spodni");
+        double price = scanner.nextDouble();
+
+        //System.out.println(String.format());
+    }
+}
