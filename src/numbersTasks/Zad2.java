ed47e97a97737f0685f47656d334518e4a629a0e
Adrian Langier <langier.adrian@gmail.com>
2021-03-15 19:42:31 +0100

    additional numbers Task2

---
+++ src/numbersTasks/Zad2.java
@@ -0,0 +1,9 @@
+package numbersTasks;
+
+public class Zad2 {
+    public static void main(String[] args) {
+        short a =5;
+        short b = 10;
+        int c = a+b;   // <--- Java wymaga typu "int" w tym przypadku
+    }
+}
