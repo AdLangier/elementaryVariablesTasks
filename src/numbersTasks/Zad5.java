a8878aa51e5afa503366285f82922ca2bfcc4fef
Adrian Langier <langier.adrian@gmail.com>
2021-03-15 20:37:48 +0100

    additional numbers Task5

---
+++ src/numbersTasks/Zad5.java
@@ -0,0 +1,13 @@
+package numbersTasks;
+
+import java.util.Scanner;
+
+public class Zad5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj promień koła w cm");
+        int r = scanner.nextInt();
+        double circleField = r*r*Math.PI;
+        System.out.println("Pole koła to "+String.format("%.2f",circleField)+" cm2"); // z zaokrągleniem do 2 miejsc po przecinku
+    }
+}
