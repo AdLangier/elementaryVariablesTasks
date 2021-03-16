fab34819911aeeab4706bc9692f6ea4645f8a6ef
Adrian Langier <langier.adrian@gmail.com>
2021-03-16 12:53:26 +0100

    additional numbers Task11

--- src/numbersTasks/Zad11.java
+++ src/numbersTasks/Zad11.java
@@ -1,13 +1,22 @@
 package numbersTasks;
 
 import java.util.Scanner;
-
+// ZADANIE BARDZIEJ NADAJĄCE SIĘ DO ZADAŃ GRUPY STRING...
 public class Zad11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj dokładną cenę spodni");
-        double price = scanner.nextDouble();
+        String price = scanner.next();                                       // używamy typu String ponieważ łatwiej jest go formatować
 
-        //System.out.println(String.format());
+        if(price.indexOf(",")!=-1){                                         //Trudne!! - do ogarnięcia metody Stringa
+            System.out.println("Cena spodni to: ");
+            System.out.println(price.substring(0,price.indexOf(",")).replace(",",""));
+            System.out.println(price.substring(price.indexOf(",")).replace(",",""));
+        }else{
+            System.out.println("Cena to: ");
+            System.out.println(price);
+        }
+
+
     }
 }
