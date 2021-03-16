a123ed0400fa78adad596f3297b5c46f5cd985ec
Adrian Langier <langier.adrian@gmail.com>
2021-03-16 13:59:32 +0100

    additional String Task2

--- src/stringTasks/Zad2.java
+++ src/stringTasks/Zad2.java
@@ -1,4 +1,19 @@
 package stringTasks;
 
+import java.util.Scanner;
+
 public class Zad2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj wyraz");
+        String word = scanner.next();
+        int length = word.length();                    //WAŻNE! - poniżej kolejność podania warunków logicznych najpierw && potem ||
+
+        if (length % 10 == 2 && length != 12|| length % 10 == 3 && length != 13 || length % 10 == 4 && length != 14) {
+            System.out.println("Długość podanego wyrazu to " + length + " litery");
+        } else {
+            System.out.println("Długość podanego wyrazu to " + length + " liter");
+        }
+    }
 }
+//dodatkowo - dodać warunek, który będzie poprawnie formatował słowo "litery"
