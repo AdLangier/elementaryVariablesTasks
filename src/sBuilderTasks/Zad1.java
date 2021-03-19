ce818c649a3e590e106b8687c1f470f41d430b15
Adrian Langier <langier.adrian@gmail.com>
2021-03-19 11:41:03 +0100

    additional sBuilder Task 1

---
+++ src/sBuilderTasks/Zad1.java
@@ -0,0 +1,14 @@
+package sBuilderTasks;
+
+import java.util.Scanner;
+
+public class Zad1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj wyraz");
+        String word = scanner.next();
+
+        System.out.println("Pierwsza litera tego wyrazu to "+word.charAt(0));
+    }
+
+}
