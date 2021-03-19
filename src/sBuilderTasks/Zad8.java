599f3bc523ca98fc3f086226d5b565bcb7e145ab
Adrian Langier <langier.adrian@gmail.com>
2021-03-19 18:16:59 +0100

    additional sBuilder Task 8

---
+++ src/sBuilderTasks/Zad8.java
@@ -0,0 +1,14 @@
+package sBuilderTasks;
+
+import java.util.Scanner;
+
+public class Zad8 {                                              //Trudne -- metody klasy String
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wpisz dane piłkarza w formacie: Imię i Nazwisko, Liczba Goli");
+        String playerData = scanner.nextLine();
+        int a = playerData.indexOf(" ");
+        int b = playerData.indexOf(",");
+        System.out.println("Nazwisko: "+playerData.substring(a,b));
+    }
+}
