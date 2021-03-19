7feab8f5a489d20694be75e0427d6db487bd3a9e
Adrian Langier <langier.adrian@gmail.com>
2021-03-19 18:35:23 +0100

    additional sBuilder Task 13

---
+++ src/sBuilderTasks/Zad13.java
@@ -0,0 +1,12 @@
+package sBuilderTasks;
+
+public class Zad13 {
+    public static void main(String[] args) {
+
+        String name = "Adrian";
+
+        int a = 0;
+
+        System.out.println(name.charAt(a));    //Pierwsza litera bo a ma wartość 0:)
+    }
+}
